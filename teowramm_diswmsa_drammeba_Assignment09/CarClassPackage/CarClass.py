cd054c2cdd26846f3d4f58e05265a2b705113f68
smita <smita@DESKTOP-L3NU2O8.wifi.uc.edu>
2024-03-28 17:51:44 -0400

    4th commit by smita

--- teowramm_diswmsa_drammeba_Assignment09/CarClassPackage/CarClass.py
+++ teowramm_diswmsa_drammeba_Assignment09/CarClassPackage/CarClass.py
@@ -12,6 +12,16 @@ class Car:
             'chevrolet': 'https://api.api-ninjas.com/v1/cars?model=chevrolet'
         }
         self.car_models = list(self.model_urls.keys()) #getting dictionary keys
-
+        
+    def get_car_info(self, model):
+        api_url = f'https://api.api-ninjas.com/v1/cars?model=model'
+        response = requests.get(api_url, headers={'X-Api-Key': self.api_key})
+        if response.status_code == requests.codes.ok:
+            return json.loads(response.content)
+        #https://www.geeksforgeeks.org/json-loads-in-python/
+        # https://www.geeksforgeeks.org/how-to-parse-data-from-json-into-python/?ref=ml_lbp
+        #https://www.geeksforgeeks.org/convert-json-to-dictionary-in-python/?ref=ml_lbp
+        else:
+            return None
         
         
